387293d595995e3103ba2366874073a0d6bdb5cb
Daniel Lee <osdaniellee@gmail.com>
2015-09-04 13:43:56 +0800

    弄明白unsigned int 和int转化中二进制负数的影响

--- nowcoder/bit_oper.cpp
+++ nowcoder/bit_oper.cpp
@@ -1,10 +1,15 @@
 #include <iostream>
+#include <stdio.h>
 
 using namespace std;
 
+// 通过unsigned int 将原来的负数转化为正数，但是在二进制表示中仍然是一样的
+
 int main(){
-	int i = -1;
+	int i = -2;
 	unsigned int ii = i;
-	cout << ii << endl;
+	cout << i << ","  << ii << endl;
+	printf("\n%x",i);
+	printf("\n%x",ii);
 	return 0;
 }
